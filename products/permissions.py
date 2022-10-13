abb9e4d11fb77ad2d00f7bca12b3c7732993cf18
Kritib Bhattarai <kritibbhattarai@Kritibs-MacBook-Pro.local>
2022-10-13 15:09:36 -0500

    login phase

--- products/permissions.py
+++ products/permissions.py
@@ -1,15 +1,20 @@
 from rest_framework import permissions
 
 class SuperUserAndAuthor(permissions.BasePermission):
-    edit_methods=("PUT", "PATCH" ,"DELETE")
+    edit_methods=("POST","PUT", "PATCH" ,"DELETE")
     def has_permission(self, request, view):
-        return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            if not request.user.is_authenticated:
+                return False
+            return True
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.method in self.edit_methods:
             if request.user.is_authenticated:
-               if request.user==obj.product_author or request.user.is_staff:
+               if request.user.email==obj.email or request.user.is_staff:
                    return True
             else:
                 return False
