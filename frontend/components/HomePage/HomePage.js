fc730537191d74117cf3da91b33a8797ee5fbfc0
1935-a11ly <musami01@luther.edu>
2022-11-14 08:58:31 -0600

    hppyeah

--- frontend/components/HomePage/HomePage.js
+++ frontend/components/HomePage/HomePage.js
@@ -1,25 +1,53 @@
 import Link from 'next/link'
+import styles from "./HomePage.module.css";
+import {fetcher} from "../../fetch/";
+import { useRouter } from 'next/router';
+import Image from 'next/image'
 
 export default function HomePage() {
   return (
-    <div>
-
-        <h1>
-	  Welcome to Luther Marketplace.
-        </h1>
-	  <ul>
-	  <li>
+	<><div className={styles.main}>
+		<div className={styles.Nav}>
+		  <div className={styles.NavbarContainer}>
+			  <div className={styles.NavLogo}>
+				  <a href="/">Luther Marketplace</a>
+			  </div>
+			  <h1 className={styles.menufonts}>Explore</h1>
+			  <h1 className={styles.menufonts}>Near Me</h1>
+			  <h1 className={styles.menufonts}>What's New?</h1>
+			  <h1 className={styles.signin}>Sign-in</h1>
+			  <h1 className={styles.sellfonts}><button className={styles.sellbtn}><a href="http://10.28.164.119:8000/admin/products/product/add/" target="_blank">Get Started</a></button></h1>
+			  <h1 className={styles.burgerfont}><button className={styles.burger}><a href="http://10.28.164.119:8000/admin/products/product/add/" target="_blank">Menu</a></button></h1>
+		  </div>
+	  </div>
+	  <div className={styles.midsection}>
+	  <div className={styles.lutherbell}></div>
+	  <div className={styles.commute}>127<br></br><h1 className={styles.commtxt}>Commuters</h1></div>
+	  <div className={styles.sales}>Sales<br></br><h1 className={styles.salestxt}>512,030</h1><h2 className={styles.gains}>+41.3%</h2></div>
+	  			<h1 className={styles.luthercomm}>
+				  LUTHER COMMUNITY
+			 	</h1>
+				 <h1 className={styles.lutherstate}>
+				  Services that our<br></br> community needs<br></br> most
+			 	</h1>
+				 <h1 className={styles.getstarted}><button className={styles.starter}><a href="http://10.28.164.119:8000/admin/products/product/add/" target="_blank">Get Started</a></button></h1>
+	  </div>
+	  <div className={styles.format}>
+	  <div className={styles.backlayerimage}>
+	  <div class={styles.imglayer2}>
+	  </div>
 	  <Link href="/login">
-	  Login
+				<a className={styles.linklayer1}> RideShare </a>
 	  </Link>
-	  </li>
-	  <li>
-	  <Link href="/shop">
-	  Shop 
+	  </div>
+	  <div className={styles.backlayerimage2}>
+	  <div class={styles.imglayer}>
+	  </div>
+	  <Link className={styles.linklayer} href="/shop">
+				<a className={styles.linklayer2}> Shop </a>
 	  </Link>
-	  </li>
-	  </ul>
-
-    </div>
+	  </div>
+	  </div>
+</div></>
   )
 }
--- frontend/components/Login/Login.js
+++ frontend/components/Login/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
 import {fetcher} from "../../fetch/";
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 export default function Login() {
 	const router=useRouter()
@@ -19,7 +19,7 @@ export default function Login() {
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		const response = await fetcher("token/", "POST", 'application/json',JSON.stringify(fields));
+		const response = await fetcher("token/", "POST", fields);
 		console.log(response);
 		localStorage.setItem('access_token', response.access);
 		localStorage.setItem('refresh_token', response.refresh)
@@ -28,27 +28,51 @@ export default function Login() {
 
 	return (
 		<main className={styles.main}>
+			<h1><a href="/" className={styles.marketplace}>Luther Marketplace</a></h1>
+			<div className={styles.upperbody}>
+			<div className={styles.welcomebox}>
+			<br></br>
+            <h1 className={styles.welcome}>Welcome Back</h1>
+            <h2 className={styles.details}>Please enter your details.</h2>
 			<form>
-				<label htmlFor="email">Email</label>
-				<input
+				<input className={styles.email}
 					type="text"
 					id="email"
 					name="email"
+					placeholder="Email"
 					required
 					onChange={handleChange}
 				/>
-				<label htmlFor="password">Password</label>
-				<input
+				<br></br>
+				<input className={styles.password}
 					type="password"
+					placeholder="Password"
 					id="password"
 					name="password"
 					required
 					onChange={handleChange}
 				/>
-				<button type="button" onClick={handleSubmit}>
-					Submit
+				<br></br>
+				<input className={styles.checkbox}
+					type="checkbox"
+					id="remmeberme"
+					name="remmeberme"
+					required
+					onChange={handleChange}
+				/>
+				<label htmlFor="remmeberme" className={styles.checklabel}>Remember Me</label>
+				<h4 className={styles.forgot}>Forgot Password</h4>
+            	<br></br>
+				<button className={styles.submit} type="button" onClick={handleSubmit}>
+					Sign in
 				</button>
+            	<h4 className={styles.h4txt}>Don't have an account?<a href="/">Create an account</a></h4>
 			</form>
+			</div>
+        </div>
+		<div className={styles.imghalf}>
+        	<img className={styles.structure} src="https://ak1.ostkcdn.com/images/products/is/images/direct/99e2126500ab99264a06f7bd2ddf7112a46dcb21/Art-Leon-Mid-century-3-seat-Sofa.jpg"></img>
+    	</div>
 		</main>
 	);
 }
